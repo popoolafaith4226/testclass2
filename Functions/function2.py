fb1e6c1ca18d488ac291aa959a2aeae3dcffa663
Popoola Faith <popoolafaith177@gmail.com>
2024-11-03 02:04:20 +0100

    commit3

--- Functions/function2.py
+++ Functions/function2.py
@@ -1,56 +1,18 @@
-def my_function():
-    print("Hello good morning")
+if 5 > 2:
+ print("Five is greater than two!")
 
-my_function()
+# variables in python
+x = 5
+y = "Hello, World!"
 
-def my_function(fname):
-    print(fname + " Hello")
+#This is a comment.
+print("Good morning Test")
 
-my_function("monday")
-my_function("Tuesday")
-my_function("wednesday")
-
-
-#This function expects 2 arguments, and gets 2 arguments:
-
-def my_function(fname, lname):
-  print(fname + " " + lname)
-
-my_function("Emil", "Refsnes")
-
-
-def my_function(*kids):
-  print("The youngest child is " + kids[0])
-
-my_function("Emil", "Tobias", "Linus")
-
-
-#Default Parameter Value
-
-def my_function(country = "Norway"):
-  print("I am from " + country)
-
-my_function("Sweden")
-my_function("India")
-my_function()
-my_function("Brazil")
-
-
-def my_function(country = " Nigeria"):
-    print("I am form" + country)
-my_function()
-my_function(" lagos")
-
- #Passing a List as an Argument
-
-def my_function(food):
-  for x in food:
-    print(x)
-
-fruits = ["apple", "banana", "cherry"]
-
-my_function(fruits)
-
-hello
+#commenting out longer lines of code
+"""
+This is a comment
+written in
+more than just one line
+"""
 
 
