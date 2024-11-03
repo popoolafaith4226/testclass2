7944d5528444cb3c0fc0a4f5853c51912109ce6b
Popoola Faith <popoolafaith177@gmail.com>
2024-11-03 02:15:22 +0100

    commit 4

--- Functions/pythonfunction.py
+++ Functions/pythonfunction.py
@@ -1,34 +1,48 @@
 def my_function():
-    print("Hello good morning")
+   print("Hello good morning")
+
 
 my_function()
 
+
 def my_function(fname):
-    print(fname + " Hello")
+   print(fname + " Hello")
+
 
 my_function("monday")
 my_function("Tuesday")
 my_function("wednesday")
 
 
+
+
 #This function expects 2 arguments, and gets 2 arguments:
 
+
 def my_function(fname, lname):
-  print(fname + " " + lname)
+ print(fname + " " + lname)
+
 
 my_function("Emil", "Refsnes")
 
 
+
+
 def my_function(*kids):
-  print("The youngest child is " + kids[0])
+ print("The youngest child is " + kids[0])
+
 
 my_function("Emil", "Tobias", "Linus")
 
 
+
+
 #Default Parameter Value
 
+
 def my_function(country = "Norway"):
-  print("I am from " + country)
+ print("I am from " + country)
+
 
 my_function("Sweden")
 my_function("India")
@@ -36,20 +50,23 @@ my_function()
 my_function("Brazil")
 
 
+
+
 def my_function(country = " Nigeria"):
-    print("I am form" + country)
+   print("I am form" + country)
 my_function()
 my_function(" lagos")
 
- #Passing a List as an Argument
+
+#Passing a List as an Argument
+
 
 def my_function(food):
-  for x in food:
-    print(x)
+ for x in food:
+   print(x)
+
 
 fruits = ["apple", "banana", "cherry"]
 
+
 my_function(fruits)
-
-
-
